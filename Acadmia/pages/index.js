9891b64f65979f7c606a4f39408248e32a6e52ce
Amr <Amr@DESKTOP-5F788VM>
2021-05-14 11:20:51 +0200

    Add project files.

---
+++ Acadmia/pages/index.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+
+
+
+    $('#carouselExampleIndicators').carouFredSel({
+        width: '90%',
+        items: 3,
+        scroll: 3,
+        auto: false,
+      
+        infinite: false
+    });
+
+
+})
